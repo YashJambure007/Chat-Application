903300ee55b5f00d3e2ed3a27f97a8e7e20d275d
YashJambure007 <jambureyash2003@gmail.com>
2025-06-16 23:26:36 +0530

    chat application

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+/* eslint-disable react/prop-types */
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,17 +9,60 @@ import { Toaster } from "react-hot-toast";
 import { persistor, store } from "./redux/Store";
 import { PersistGate } from "redux-persist/integration/react";
 
+// Protected route wrapper
+const ProtectedRoute = ({ children }) => {
+  const user = useSelector((state) => state.user?.user);
+  return user ? children : <Navigate to="/login" replace />;
+};
+
+// Redirect if logged in
+const PublicRoute = ({ children }) => {
+  const user = useSelector((state) => state.user?.user);
+  return user ? <Navigate to="/home" replace /> : children;
+};
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+
+      <Route
+        path="/register"
+        element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        }
+      />
+
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+}
+
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Toaster />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
+          <AppRoutes />
         </BrowserRouter>
       </PersistGate>
     </Provider>
